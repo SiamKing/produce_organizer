e7f2639d7dd0c4be27d4517119f43e8983f711f6
SiamKing <thesiameseelectric@gmail.com>
2016-06-04 18:32:45 -0700

    added patch path to produce database controller

--- app/controllers/produce_database_controller.rb
+++ app/controllers/produce_database_controller.rb
@@ -3,9 +3,31 @@ require 'rack-flash'
 class ProduceDatabaseController < ApplicationController
 
 
-    get '/produce_database/edit' do
-      erb :'/produce_database/edit'
+  get '/produce_database/edit' do
+    erb :'/produce_database/edit'
+  end
+
+  patch '/produce_database/:id' do
+    #binding.pry
+    if params[:name] != "" && params[:shelf_life] != ""
+      produce = ProduceDatabase.find(params[:id])
+      produce.name = params[:name]
+      produce.shelf_life = params[:shelf_life]
+      produce.update
+    elsif params[:name] != ""
+      produce = ProduceDatabase.find(params[:id])
+      produce.name = params[:name]
+      produce.update
+    elsif params[:shelf_life] != ""
+      produce = ProduceDatabase.find(params[:id])
+      produce.shelf_life = params[:shelf_life]
+      produce.update
+    else
+      redirect '/produce_database/edit'
     end
+    flash[:notice] = "You successfully changed the item"
+    redirect '/users/user'
+  end
 
 
 end
