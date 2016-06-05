01f98692c801d9b1866e2f062e5b11c121efc058
SiamKing <thesiameseelectric@gmail.com>
2016-06-04 22:04:39 -0700

    added flash notices

--- app/controllers/produce_database_controller.rb
+++ app/controllers/produce_database_controller.rb
@@ -10,19 +10,24 @@ class ProduceDatabaseController < ApplicationController
 
   patch '/produce_database/:id' do
     produce = ProduceDatabase.find(params[:id])
-    if params[:name] != "" && params[:shelf_life] != ""
-      produce.name = params[:name]
-      produce.shelf_life = params[:shelf_life]
-      produce.save
-    elsif params[:name] != ""
-      produce.name = params[:name]
-      produce.save
-    elsif params[:shelf_life] != "" && params[:shelf_life].to_i.class == Fixnum
-      produce.shelf_life = params[:shelf_life]
-      produce.save
+    if current_user.id == ProduceDatabase.user_id
+      if params[:name] != "" && params[:shelf_life] != ""
+        produce.name = params[:name]
+        produce.shelf_life = params[:shelf_life]
+        produce.save
+      elsif params[:name] != ""
+        produce.name = params[:name]
+        produce.save
+      elsif params[:shelf_life] != "" && params[:shelf_life].to_i.class == Fixnum
+        produce.shelf_life = params[:shelf_life]
+        produce.save
+      else
+        flash[:notice] = "Nothing was changed. Please Edit Below"
+        redirect '/produce_database/edit'
+      end
     else
-      flash[:notice] = "Nothing was changed. Please Edit Below"
-      redirect '/produce_database/edit'
+      flash[:notice] = "Only the user that created the item can edit it."
+      redirect '/users/user'
     end
     flash[:notice] = "You successfully changed the item"
     redirect '/produce_database/edit'
@@ -30,6 +35,10 @@ class ProduceDatabaseController < ApplicationController
 
   get '/produce_database/new' do
     erb :'/produce_database/new'
+  end
+
+  post '/produce_database/new' do
+
   end
 
 end
